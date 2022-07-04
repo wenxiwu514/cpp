6b8c0af403ede23bfef06cdc7f3a1a6caadf4258
syaox <wenxiwu514@gmail.com>
2022-07-04 14:41:43 +0800

    1

---
+++ acwing/ac787归并.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int a[100010], tmp[100010];
+
+void merge_sort(int q[], int l, int r)
+{
+    if (l >= r)
+        return;
+    int mid = (l + r) >> 1;
+    merge_sort(q, l, mid);
+    merge_sort(q, mid + 1, r);
+
+    int k = 0, i = l, j = mid + 1;
+    while (i <= mid && j <= r)
+    {
+        if (q[i] <= q[j])
+        {
+            tmp[k++] = a[i++];
+        }
+        else
+        {
+            tmp[k++] = a[j++];
+        }
+    }
+    while (i <= mid)
+        tmp[k++] = a[i++];
+    while (j <= r)
+        tmp[k++] = a[j++];
+    for (int i = l, j = 0; i <= r; i++, j++)
+        a[i] = tmp[j];
+}
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    merge_sort(a, 0, n - 1);
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    return 0;
+}
